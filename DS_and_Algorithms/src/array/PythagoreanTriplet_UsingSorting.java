d5f133136d48bbbd298c0350d98dd7c8b3479b3c
Abhishek Singh <abhiwanttochat@gmail.com>
2019-08-14 09:25:48 +0530

    Find Pythagorean triplet in an unsorted array - using sorting

--- DS_and_Algorithms/src/array/PythagoreanTriplet_UsingSorting.java
+++ DS_and_Algorithms/src/array/PythagoreanTriplet_UsingSorting.java
@@ -2,6 +2,8 @@ package array;
 
 import java.util.Arrays;
 
+
+//O(n^2)
 public class PythagoraeanTriplet_UsingSorting {
 	public static void main(String[] args) {
 		int[] arr = {2,8,4,7,5,9,3};
@@ -10,12 +12,15 @@ public class PythagoraeanTriplet_UsingSorting {
 	}
 
 	private static boolean hasPythagoreanTriplet(int[] arr, int n) {
+		//this step takes O(n)
 		for(int i=0; i<n; i++) {
 			arr[i]= arr[i]* arr[i];
 		}
 		
+		//this step takes O(nlogn)
 		Arrays.sort(arr);
 		
+		//this step takes O(n^2): n for for loop and n for while loop
 		for(int i=n-1; i>2; i--) {
 			int l=0; 
 			int r= i-1;
