f59c146de3ab14af1629440049e6f9afd8ecc126
Abhishek Singh <abhiwanttochat@gmail.com>
2021-01-28 08:53:07 +0530

    Recursion: Factorial of a number

---
+++ DS_and_Algorithms/src/recursion/Recursion_Factorial.java
@@ -0,0 +1,16 @@
+package recursion;
+
+public class Recursion_Factorial {
+	public static void main(String[] args) {
+		System.out.println(factorial(4));
+	}
+
+	private static int factorial(int n) {
+		if(n==0) {
+			return 1;
+		}else {
+			return n*factorial(n-1);
+		}
+		
+	}
+}
